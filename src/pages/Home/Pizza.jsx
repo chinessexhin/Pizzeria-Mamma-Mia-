d3ab4bbe34cd476dc0d940ffbfc99d8bf470980d
aidem <167827069+chinessexhin@users.noreply.github.com>
2025-03-17 00:23:27 -0300

    Update Pizza.jsx

--- src/pages/Home/Pizza.jsx
+++ src/pages/Home/Pizza.jsx
@@ -1,8 +1,46 @@
-const Pizza = () => {
-  return (
-    <div>
-    </div>
-  )
-}
+import { useParams } from 'react-router-dom';
+import { useContext } from 'react';
+import { Context } from './context/CardContext';
+import { Container, Card, Button } from 'react-bootstrap';
 
-export default Pizza
+export default function Pizza() {
+  const { id } = useParams();
+  const { pizzas, addCart } = useContext(Context);
+
+  return (
+    <Container>
+      {pizzas
+        .filter((pizza) => pizza.id === id)
+        .map((pizza, index) => (
+          <Card key={index} className="row flex-row py-3">
+            <Container className="col-12 col-md-4">
+              <Card.Img src={pizza.img} alt={pizza.name} />
+            </Container>
+            <Container className="col-12 col-md-8">
+              <Card.Header>
+                <span className="text-capitalize fw-bold fs-5">
+                  {pizza.name}
+                </span>
+              </Card.Header>
+              <Card.Body>
+                <Card.Text>{pizza.desc}</Card.Text>
+                Ingredientes:
+                <ul>
+                  {pizza.ingredients.map((ingrediente, index) => (
+                    <li key={index}>🍕 {ingrediente}</li>
+                  ))}
+                </ul>
+              </Card.Body>
+              <Card.Footer className="text-center">
+                <Card.Text className="d-flex justify-content-around align-items-center">
+                  <Button onClick={() => addCart(pizza)}>
+                    Añadir <IconCart tamaño=".9rem" color="white" />
+                  </Button>
+                </Card.Text>
+              </Card.Footer>
+            </Container>
+          </Card>
+        ))}
+    </Container>
+  );
+}
