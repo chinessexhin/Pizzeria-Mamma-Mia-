d7e5f8150cc6c3fbef6f883ee562a582d0765fb9
aidem <167827069+chinessexhin@users.noreply.github.com>
2025-03-17 00:57:05 -0300

    Update PizzaCarrito.jsx

--- src/components/PizzaCarrito.jsx
+++ src/components/PizzaCarrito.jsx
@@ -1,42 +1,46 @@
-rt { Table, Button } from 'react-bootstrap';
-import { useContext } from 'react';
-import { Context } from '../contexts/CartContext';
+import { Button } from "react-bootstrap";
+import { useContext } from "react";
+import { Context } from "../contexts/CartContext";
+
 
 export default function PizzasCarrito() {
-  const { cart, decreaseCount, increaseCount, totalCart } =
-    useContext(Context);
+  const { cart, decreaseCount, increaseCount, totalCart } = useContext(Context);
 
   return (
-    <>
-      <Table responsive>
-        <tbody>
-          {cart.map((pizza, index) => (
-            <tr key={index}>
-              <td>
-                <img src={pizza.img} alt={pizza.name} />
-              </td>
-              <td className="w-75 text-capitalize">{pizza.name}</td>
-              <td>
-                <Button onClick={() => decreaseCount(index)}>-</Button>
-              </td>
-              <td>{pizza.count}</td>
-              <td>
-                <Button onClick={() => increaseCount(index)}>+</Button>
-              </td>
-              <td>=</td>
-            </tr>
-          ))}
-        </tbody>
-        <tfoot>
-          <tr>
-            <td colSpan="5" className="text-end fw-bold">
-              Total
-            </td>
-            <td>=</td>
-          </tr>
-        </tfoot>
-      </Table>
-      <Button className="float-end">Ir a pagar</Button>
-    </>
+    <div className="cart-container">
+      {cart.map((pizza, index) => (
+        <PizzaItem
+          key={pizza.id || index}
+          pizza={pizza}
+          onDecrease={() => decreaseCount(index)}
+          onIncrease={() => increaseCount(index)}
+        />
+      ))}
+      <TotalRow total={totalCart} />
+      <Button className="checkout-button">Ir a pagar</Button>
+    </div>
+  );
+}
+
+function PizzaItem({ pizza, onDecrease, onIncrease }) {
+  return (
+    <div className="pizza-item">
+      <img src={pizza.img} alt={pizza.name} className="pizza-img" />
+      <span className="pizza-name">{pizza.name}</span>
+      <div className="counter">
+        <Button onClick={onDecrease}>-</Button>
+        <span className="pizza-count">{pizza.count}</span>
+        <Button onClick={onIncrease}>+</Button>
+      </div>
+    </div>
+  );
+}
+
+function TotalRow({ total }) {
+  return (
+    <div className="total-row">
+      <span className="total-text">Total:</span>
+      <span className="total-amount">{monedaLocal(total)}</span>
+    </div>
   );
 }
