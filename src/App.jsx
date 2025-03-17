9ede3f8ae6241dd6c17446fb760c00b62170d4ab
aidem <167827069+chinessexhin@users.noreply.github.com>
2025-03-17 00:08:03 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,6 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import CartProvider from './context/CartContext';
+
 import Navbar from './components/Navbar/Navbar'
 import Footer from './components/Footer/Footer'
 import Home from './pages/Home/Home'
@@ -5,28 +8,28 @@ import Cart from './pages/Cards/Cart'
 import Login from './pages/Login/Login'
 import Registro from './pages/Register/Registro'
 import NotFound from './pages/Notfound/Notfound'
-import { Route, Routes } from 'react-router-dom'
+
 
 const App = () => {
   return (
-  <>
-   <Navbar />
+   <CartProvider>
+     <BrowserRouter>
+      <Navbar />
 
-    <Routes>
-      <Route path='/' element= {<Home />} />
-      <Route path='/login' element= {<Login />} />
-      <Route path='/register' element= {<Registro />} />
-      <Route path='/cart' element= {<Cart />} />
-      {/* <Route path='/pizza/p001' element= {<Pizza />} /> */}
-      {/* <Route path='/profile' element= {<Profile />} /> */}
-      <Route path='/404' element= {<NotFound />} />
-    </Routes>
+       <Routes>
+         <Route path='/' element= {<Home />} />
+         <Route path='/login' element= {<Login />} />
+         <Route path='/register' element= {<Registro />} />
+         <Route path='/cart' element= {<Cart />} />
+         <Route path='/404' element= {<NotFound />} />
+       </Routes>
 
-    <Home />
+        <Home />
 
-   <Footer />
-  </>
-  )
+       <Footer />
+    </BrowserRouter>
+  <CartProvider/>
+  );
 }
 
 export default App
