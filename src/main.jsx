c30367cf03bc79ac1ca83338c24bca3a6a91836c
aidem <167827069+chinessexhin@users.noreply.github.com>
2025-03-17 00:27:47 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,20 +4,10 @@ import './main.css'
 //import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import { BrowserRouter } from "react-router-dom";
 import React from 'react';
-import CartProvider from './context/CartContext.jsx';
-import UserProvider from './context/UserContext.jsx';
-
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-     <UserProvider>
-       <CartProvider>
         <App />
-       </CartProvider>
-     </UserProvider>
-    </BrowserRouter>
   </React.StrictMode>
 )
