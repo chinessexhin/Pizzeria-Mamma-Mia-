2ca7c4ee76da293af640d2f7b17138fd4a0e3127
aidem <167827069+chinessexhin@users.noreply.github.com>
2025-03-27 20:49:04 -0300

    Update Cardpizza.jsx

--- src/pages/Cards/Cardpizza.jsx
+++ src/pages/Cards/Cardpizza.jsx
@@ -1,20 +1,23 @@
-const Card = ({ img, title, text, button, price, button2 }) => {
+import React from "react";
+
+const Cardpizza = ({ img, title, text, price, button, button2, className, style }) => {
+  const formattedPrice = new Intl.NumberFormat("es-CL", {
+    style: "currency",
+    currency: "CLP"
+  }).format(price);
+
   return (
-    <div className="card">
+    <div className={`card ${className}`} style={style}>
       <img src={img} className="card-img-top" alt={title} />
       <div className="card-body">
         <h5 className="card-title">{title}</h5>
         <p className="card-text">{text}</p>
-        <p className="card-text">{price}</p>
-        <p className="card-text">
-        <div className="button-container">
-          <button type="button" className="btn btn-dark">{button}</button>
-          <button type="button" className="btn btn-dark">{button2}</button>
-        </div>
-        </p>
+        <p className="card-price">{formattedPrice}</p>
+        <button className="btn btn-primary">{button}</button>
+        <button className="btn btn-success ms-2">{button2}</button>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Card
+export default Cardpizza;
