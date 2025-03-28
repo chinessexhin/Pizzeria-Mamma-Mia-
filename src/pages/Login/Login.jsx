2e0cc8f51728316dd983c14886d60d6684909c7d
aidem <167827069+chinessexhin@users.noreply.github.com>
2025-03-28 01:51:23 -0300

    Update Login.jsx

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,17 +1,43 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState, useContext } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { UserContext } from '../context/UserProvider';
 
 const Login = () => {
+  const { login } = useContext(UserContext);
+  const navigate = useNavigate();
+  const [credentials, setCredentials] = useState({
+    email: '',
+    password: '',
+  });
+
+  const handleChange = (e) => {
+    setCredentials({
+      ...credentials,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const success = await login(credentials);
+    if (success) {
+      navigate('/dashboard'); 
+    } else {
+      alert('Incorrecto'); 
+    }
+  };
+
   return (
-    <>
     <div>
-        <form className="formulario">
+      <form className="formulario" onSubmit={handleSubmit}>
         <div className="form-group mb-3">
           <input
             type="email"
             name="email"
             className="form-control"
             placeholder="tuemail@ejemplo.com"
+            value={credentials.email}
+            onChange={handleChange}
           />
         </div>
         <div className="form-group mb-3">
@@ -20,20 +46,22 @@ const Login = () => {
             name="password"
             className="form-control"
             placeholder="Contraseña"
+            value={credentials.password}
+            onChange={handleChange}
           />
         </div>
         <div className="d-grid gap-2">
           <button type="submit" className="btn btn-success">
-            Registrarse
+            Iniciar sesión
           </button>
         </div>
-     </form>
-     <p className="mt-3 text-center">
-       ¿No cuentas con una cuenta? <Link to="/"> Registtrate!</Link>
-     </p>
+      </form>
+      <p className="mt-3 text-center">
+        ¿No tienes una cuenta? <Link to="/register">Regístrate aquí</Link>
+      </p>
     </div>
-    </>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
+
